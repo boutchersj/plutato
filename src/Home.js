7b5816ca57195daf5d425e97f21faa42382e986a
Steven Boutcher <boutchersj@gmail.com>
2022-07-02 03:32:07 -0500

    Add flashcard component

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth } from './Firebase';
+import Flashcard from './Flashcard';
 
 function Home() {
     const navigate = useNavigate();
@@ -15,9 +16,14 @@ function Home() {
             })
     }
     return (
-        <div id='home' className="flex bg-purple-400 h-[100vh]">
-            <h1 className="font-bold text-2xl ml-auto">Plutato</h1>
-            <button className="border border-white rounded-lg bg-red-500 font-semibold text-white h-12 w-24 ml-auto" onClick={logout}>Logout</button>
+        <div id='home' className="flex-col bg-purple-400 h-[100vh]">
+            <header className="flex items-center bg-white">
+                <h1 className="font-bold text-2xl ml-auto">Plutato</h1>
+                <button className="border border-white rounded-lg bg-red-500 font-semibold text-white h-12 w-24 ml-auto" onClick={logout}>Logout</button>
+            </header>
+            <section className="flex justify-center items-center h-[50%]">
+                <Flashcard />
+            </section>
         </div>
     )
 }
