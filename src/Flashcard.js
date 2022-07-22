9ecec87812edd523490b3a2f8978c789671077cb
Steven Boutcher <boutchersj@gmail.com>
2022-07-22 06:49:30 -0500

    Give the grid some responsive sizing; add background color to the header and card grid; add background and text color to each side of the flashcard

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 
 function Flashcard(props) {
-    const [side,setSide] = useState(true)
+    const [side,setSide] = useState(true);
     const flipCard = () => {
         setSide(!side);
-    }
+    };
+    const cardColor = side ? 'bg-black text-white' : 'bg-white text-black';
 
     return (
-        <div className="flex justify-center items-center p-16 text-xl border border-black w-48 h-48" onClick={flipCard}>
+        <button className={`${cardColor} flex justify-center items-center p-16 text-xl border border-black w-48 h-48`} onClick={flipCard}>
             <p>{side ? props.nameSide : props.codeSide}</p>
-        </div>
-    )
+        </button>
+    );
 }
 
 export default Flashcard;
--- src/Home.js
+++ src/Home.js
@@ -18,11 +18,11 @@ function Home() {
 
     return (
         <div id='home' className="flex-col h-full">
-            <header className="flex items-center">
+            <header className="flex items-center bg-gray-300 p-5">
                 <h1 className="font-bold text-4xl ml-auto">Plutato</h1>
                 <button className="border border-white rounded-lg bg-red-500 font-semibold text-white h-12 w-24 ml-auto" onClick={logout}>Logout</button>
             </header>
-            <section className="flex justify-center items-center h-full p-10">
+            <section className="flex justify-center items-center h-full p-10 bg-emerald-400">
                 <Flashcards deck="units-misc" />
             </section>
         </div>
